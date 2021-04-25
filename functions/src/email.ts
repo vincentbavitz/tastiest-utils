15f55a73d7fff306c9d8a16e1b55eb0cbf93f4d3
Vincent Bavitz <vincent@bavitz.org>
2021-04-26 02:30:19 +1000

    Dashbboard restaurant onboarding

--- functions/src/email.ts
+++ functions/src/email.ts
@@ -1,1 +1,40 @@
-export const sth = 'fg';
+import {
+  FirestoreCollection,
+  RestaurantData,
+  RestaurantDataApi,
+  UserRole,
+} from '@tastiest-io/tastiest-utils';
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Analytics = require('analytics-node');
+const analytics = new Analytics(functions.config().segment.write_key);
+
+/**
+ * Restaurant onbaording flow.
+ */
+export const onboardingRestaurant = functions.firestore
+  .document(`${FirestoreCollection.RESTAURANTS}/{restaurantId}`)
+  .onCreate(async (snap, context) => {
+    const { restaurantId } = context.params ?? '';
+
+    // Track restaurant onboarding
+    analytics.track({
+      userId: restaurantId,
+      event: `Restauraut onboarded`,
+      properties: {},
+    });
+
+    // Set TOS acceptance to false until they explicitly sign in
+    const restaurantDataApi = new RestaurantDataApi(admin, restaurantId);
+    restaurantDataApi.setRestaurantData(RestaurantData.LEGAL, {
+      hasAcceptedTerms: false,
+    });
+
+    // Set custom user claim (user role) to `restaurant`
+    // (as apposed to `eater`, `admin`).
+    admin.auth().setCustomUserClaims(restaurantId, {
+      [UserRole.RESTAURANT]: true,
+    });
+  });
