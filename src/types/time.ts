98b32579d528c90d78e81711fcc8d48f0d005790
Vincent Bavitz <vincent@bavitz.org>
2021-09-23 16:51:54 -0500

    Time typings

--- src/types/time.ts
+++ src/types/time.ts
@@ -1,13 +1,3 @@
-type ArrayLengthMutationKeys = 'splice' | 'push' | 'pop' | 'shift' | 'unshift';
-type FixedLengthArray<T, L extends number, TObj = [T, ...Array<T>]> = Pick<
-  TObj,
-  Exclude<keyof TObj, ArrayLengthMutationKeys>
-> & {
-  readonly length: L;
-  [I: number]: T;
-  [Symbol.iterator]: () => IterableIterator<T>;
-};
-
 export enum DayOfWeek {
   SUNDAY = 0,
   MONDAY = 1,
@@ -18,20 +8,23 @@ export enum DayOfWeek {
   SATURDAY = 6,
 }
 
+// Minutes into day starting at 00:00. Example: 79 --> 01:19
+export type TimeRange = [number, number];
+export type QuietTimesMetricDay = {
+  active: boolean;
+  range: TimeRange;
+  coversRequired: number; // number of covers required during quiet times
+};
+
 /** Keys are derived from DayOfWeek where SUNDAY = 0.
- *  Value represent whether or not the slot is available; 1 | 0.
- *  Since the granularity of slots is 15 minutes, we have 24 * 4 = 96
- *  items in the array, where slots[0] represents 00:00 --> 00:15,
- *  and so on.
- *  For example, to access Friday, 01:30 -> 01:45, it's slots[5][15 * 1 + 2]
+ *  Range is defined in minutes from midnight.
  */
-export type TimeSlots = FixedLengthArray<0 | 1, 96>;
 export type WeekTimeSlots = {
-  [DayOfWeek.SUNDAY]: TimeSlots;
-  [DayOfWeek.MONDAY]: TimeSlots;
-  [DayOfWeek.TUESDAY]: TimeSlots;
-  [DayOfWeek.WEDNESDAY]: TimeSlots;
-  [DayOfWeek.THURSDAY]: TimeSlots;
-  [DayOfWeek.FRIDAY]: TimeSlots;
-  [DayOfWeek.SATURDAY]: TimeSlots;
+  [DayOfWeek.SUNDAY]: QuietTimesMetricDay;
+  [DayOfWeek.MONDAY]: QuietTimesMetricDay;
+  [DayOfWeek.TUESDAY]: QuietTimesMetricDay;
+  [DayOfWeek.WEDNESDAY]: QuietTimesMetricDay;
+  [DayOfWeek.THURSDAY]: QuietTimesMetricDay;
+  [DayOfWeek.FRIDAY]: QuietTimesMetricDay;
+  [DayOfWeek.SATURDAY]: QuietTimesMetricDay;
 };
