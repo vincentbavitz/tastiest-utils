e2a2d434ca6bc3877421caf9aac25d8c310a1927
Vincent Bavitz <vincent@bavitz.org>
2022-01-15 13:15:14 -0600

    Horus SWR GET and POST

--- src/services/horus.ts
+++ src/services/horus.ts
@@ -1,58 +1,148 @@
+import firebase from 'firebase';
 import nodeFetch from 'node-fetch';
+import { useEffect, useMemo, useState } from 'react';
+import useSWR, { SWRConfiguration } from 'swr';
 import { dlog } from '..';
+
+type HorusResponse<T = any> = {
+  data: T | null;
+  error: string | null;
+};
+
+// prettier-ignore
+const TASTIEST_BACKEND_URL = ['development','test'].includes(process.env.NODE_ENV as string)
+    ? 'http://localhost:4444'
+    : 'https://api.tastiest.io';
 
 /**
  * Horus manages all interactions with the Tastiest API.
  * Server-side only (admin) endpoints are under Horus.Admin
  */
 export class Horus {
-  public Restaurant: typeof Restaurant;
+  constructor(private token: string) {}
+  // /support/restaurants/ticket/95285be1-0beb-47ea-849d-41437c4afb2c
 
-  constructor() {
-    this.Restaurant = Restaurant;
+  /**
+   * Endpoint doesn't include the base path. Eg. do /admin/users.
+   * Provide query parameters to the function rather than appending to the endponit.
+   * Eg. don't go /users?role=ADMIN, rather set { role: 'ADMIN' } as query object.
+   */
+  async get<QueryParams = Record<string, string | number>, ResponseType = any>(
+    endpoint: string,
+    query?: QueryParams,
+  ): Promise<HorusResponse<ResponseType>> {
+    const queryParams = query ?? {};
+
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const url = new URL(endpoint, TASTIEST_BACKEND_URL);
+    Object.entries(queryParams).forEach(([key, value]) => {
+      url.searchParams.set(key, value as string);
+    });
+
+    try {
+      // Use server side fetch if necessary
+      const response =
+        typeof window === 'undefined'
+          ? await nodeFetch(url.toString(), options)
+          : await fetch(url.toString(), options);
+
+      let data: ResponseType | null = null;
+      if (response.ok) {
+        try {
+          data = await response.json();
+        } catch {
+          data = null;
+        }
+      }
+
+      return { data, error: null };
+    } catch (error) {
+      return { data: null, error: String(error) };
+    }
+  }
+
+  /**
+   * Endpoint doesn't include the base path. Eg. do /admin/users.
+   */
+  async post<Params = any, ResponseType = any>(
+    endpoint: string,
+    params: Params,
+  ): Promise<HorusResponse<ResponseType>> {
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    };
+
+    dlog('horus ➡️ options:', options);
+
+    try {
+      // Use server side fetch if necessary
+      const response =
+        typeof window === 'undefined'
+          ? await nodeFetch(`${TASTIEST_BACKEND_URL}${endpoint}`, options)
+          : await fetch(`${TASTIEST_BACKEND_URL}${endpoint}`, options);
+
+      let data: ResponseType | null = null;
+      if (response.ok) {
+        try {
+          data = await response.json();
+        } catch {
+          data = null;
+        }
+      }
+
+      return { data, error: null };
+    } catch (error) {
+      return { data: null, error: String(error) };
+    }
   }
 }
 
-type RestaurantEmailScheduleParams = {
-  token: string;
-  restaurantId: string;
-  templateId: string;
-  subject: string;
-  scheduleFor: number;
-};
+const fetcher = async (url: string, token: string) => {
+  if (!token) {
+    dlog('useTastiestSWR ➡️ failed');
+    return;
+  }
 
-const Restaurant = {
-  Email: {
-    schedule: (params: RestaurantEmailScheduleParams) =>
-      buildFn('restaurants/notify', params),
-  },
-};
-
-function buildFn<T = any>(endpoint: string, params: T) {
-  return post<T>(`http://localhost:3000/${endpoint}`, { ...params });
-}
-
-async function post<Params = any>(
-  endpoint: string,
-  params: Params,
-): Promise<any> {
-  const options = {
-    method: 'POST',
-    body: JSON.stringify(params),
-  };
-
-  dlog('horus ➡️ typeof window:', typeof window);
-
-  try {
-    // Use server side fetch if necessary
-    const response =
-      typeof window === 'undefined'
-        ? await nodeFetch(endpoint, options)
-        : await fetch(endpoint, options);
-
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  }).then(response => {
     return response.json();
-  } catch (error) {
-    // throw new error();
-    return String(error);
-  }
+  });
+};
+
+/**
+ * Endpoint doesn't include the base path. Eg. do /admin/users.
+ */
+export function useHorusSWR<T>(
+  endpoint: string,
+  user: firebase.User,
+  configuration?: Partial<SWRConfiguration<T>>,
+) {
+  const [token, setToken] = useState<string | null>(null);
+
+  // Set token immediately.
+  useEffect(() => {
+    user?.getIdToken().then(setToken);
+  }, [user, endpoint, configuration]);
+
+  const path = useMemo(() => `${TASTIEST_BACKEND_URL}${endpoint}`, [endpoint]);
+  const response = useSWR<T>([path, token], fetcher, configuration);
+
+  return response;
 }
--- src/tests/services/horus.test.ts
+++ src/tests/services/horus.test.ts
@@ -12,17 +12,30 @@ describe('Test Horus', () => {
     delete global.window;
   });
 
-  test('Restaurant Emails', async () => {
-    const horus = new Horus();
+  test('Restaurant Support Get', async () => {
+    const token = `eyJhbGciOiJSUzI1NiIsImtpZCI6IjQwMTU0NmJkMWRhMzA0ZDc2NGNmZWUzYTJhZTVjZDBlNGY2ZjgyN2IiLCJ0eXAiOiJKV1QifQ.eyJyZXN0YXVyYW50Ijp0cnVlLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdGFzdGllc3QtZGlzaGVzIiwiYXVkIjoidGFzdGllc3QtZGlzaGVzIiwiYXV0aF90aW1lIjoxNjQyMjY4MjI2LCJ1c2VyX2lkIjoidUNwWWtvdkNGUlU4OGtxWFhtVng5eWxtbm04MyIsInN1YiI6InVDcFlrb3ZDRlJVODhrcVhYbVZ4OXlsbW5tODMiLCJpYXQiOjE2NDIyNzE4MzgsImV4cCI6MTY0MjI3NTQzOCwiZW1haWwiOiJkZXZlbG9wZXJzQHRhc3RpZXN0LmlvIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImRldmVsb3BlcnNAdGFzdGllc3QuaW8iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.c5KZ5lOrds2NPd6dULIhXN-5AsmQkYIPJa4LDFN5LCQlC1wNnT2Q-9iW7dKQgHqZKBN3RRyrxlhr7QEWXjaBcMP2VoV9hsxJ_ABgsqlBFvBeHtKGfH2NHrjwuz4BRRPJSirJX-3QbmGFlj97k0SCnyWI0d-4KQ7B0pPoWvaG6kZPrIw7tKMGaRtW4ptDnnVlcea-315TBU6ktkH2WUjLxwmEW81w81lxyXu3iVaP3rrSix6uXXgLdawJedfE4cjEUPupHNb6ejZU4szyy1UcPWWb8MhehAUQhRKBtonn0pN4GQ-AK4IXH5x0x5NuVXo6G6PJSFHQL-JiupCHRKVEcw`;
+    const horus = new Horus(token);
 
-    const result = await horus.Restaurant.Email.schedule({
-      token: 'vincent@bavitz.org',
-      restaurantId: 'zFekbQT8LNaQb5enmzKw5iLe46P2',
-      templateId: '1635506a-357d-426e-a8e5-efd3f3b2494e',
-      subject: 'subject33',
-      scheduleFor: Date.now() + 5000,
-    });
+    const result = await horus.get(
+      `/support/restaurants/ticket/95285be1-0beb-47ea-849d-41437c4afb2c`,
+      { key: 14, bob: 'the-builder' },
+    );
 
     dlog('horus.test ➡️ result:', result);
+
+    expect(result.error).toBeNull();
+  });
+
+  test('Restaurant Support Reply', async () => {
+    const token = `eyJhbGciOiJSUzI1NiIsImtpZCI6IjQwMTU0NmJkMWRhMzA0ZDc2NGNmZWUzYTJhZTVjZDBlNGY2ZjgyN2IiLCJ0eXAiOiJKV1QifQ.eyJyZXN0YXVyYW50Ijp0cnVlLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdGFzdGllc3QtZGlzaGVzIiwiYXVkIjoidGFzdGllc3QtZGlzaGVzIiwiYXV0aF90aW1lIjoxNjQyMjY4MjI2LCJ1c2VyX2lkIjoidUNwWWtvdkNGUlU4OGtxWFhtVng5eWxtbm04MyIsInN1YiI6InVDcFlrb3ZDRlJVODhrcVhYbVZ4OXlsbW5tODMiLCJpYXQiOjE2NDIyNzE4MzgsImV4cCI6MTY0MjI3NTQzOCwiZW1haWwiOiJkZXZlbG9wZXJzQHRhc3RpZXN0LmlvIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImRldmVsb3BlcnNAdGFzdGllc3QuaW8iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.c5KZ5lOrds2NPd6dULIhXN-5AsmQkYIPJa4LDFN5LCQlC1wNnT2Q-9iW7dKQgHqZKBN3RRyrxlhr7QEWXjaBcMP2VoV9hsxJ_ABgsqlBFvBeHtKGfH2NHrjwuz4BRRPJSirJX-3QbmGFlj97k0SCnyWI0d-4KQ7B0pPoWvaG6kZPrIw7tKMGaRtW4ptDnnVlcea-315TBU6ktkH2WUjLxwmEW81w81lxyXu3iVaP3rrSix6uXXgLdawJedfE4cjEUPupHNb6ejZU4szyy1UcPWWb8MhehAUQhRKBtonn0pN4GQ-AK4IXH5x0x5NuVXo6G6PJSFHQL-JiupCHRKVEcw`;
+    const horus = new Horus(token);
+
+    const result = await horus.post('/support/restaurants/reply', {
+      id: '95285be1-0beb-47ea-849d-41437c4afb2c',
+      name: 'Numaaan33',
+      message: 'Posting a reply again',
+    });
+
+    expect(result.error).toBeNull();
   });
 });
