09bd7b572ee34e931adfb07cbd4f39bc7e3ed54e
Vincent Bavitz <vincent@bavitz.org>
2022-01-21 03:18:07 -0600

    Payment fees fixed to 2 decimal places

--- src/utils/payments.ts
+++ src/utils/payments.ts
@@ -18,18 +18,21 @@ export function formatCurrency(value: number) {
  * We take the higher-fee structure because we can't predict in advance the card
  * being used when calculating the fees.
  *
+ * We also take an extra 10p above the maximum card fee from Stripe (20 + 10 = 30p) to ensure that
+ * internal transfers to Connect Accounts always succeeds (for when restaurant takes 100%).
+ *
  * Assumes that all values are in GBP and that the given price parameter is
  * the price after promos, discounts and etc.
  */
 export const calculatePaymentFees = (price: number) => {
-  // 2.9 % + 0.20
+  // 2.9 % + 0.30
   const PAYMENT_FEE_PERCENTAGE = 0.029;
-  const PAYMENT_FEE_FLAT_RATE = 0.2;
+  const PAYMENT_FEE_FLAT_RATE = 0.3;
 
   const fees = price * PAYMENT_FEE_PERCENTAGE + PAYMENT_FEE_FLAT_RATE;
 
   return {
-    total: price + fees,
-    fees: fees,
+    total: (price + fees).toFixed(2),
+    fees: fees.toFixed(2),
   };
 };
