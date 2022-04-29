dbeda609983bcb42bc5bca754d30b845a5d6b8e6
Vincent Bavitz <vincent@bavitz.org>
2022-04-29 03:49:28 -0500

    Routing utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './api';
 export * from './development';
 export * from './errors';
 export * from './payments';
+export * from './routing';
 export * from './text';
 export * from './time';
